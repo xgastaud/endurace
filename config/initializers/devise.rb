a84466b53b1af9ef6d4f8b7c0c7935d1165bdd71
Xavier Gastaud <xgastaud@gmail.com>
2018-03-19 13:56:33 +0100

    boilerplate endurace

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '24b47323031763c0cd239f4e0eaae6ef9197338e0b0fcbc6b88d932ce7afab842c947f5efdfc11e143587c4d01e034b01c52e9b6b4f46f3790f7ba4753ddce6d'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'xgastaud@noos.fr'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
