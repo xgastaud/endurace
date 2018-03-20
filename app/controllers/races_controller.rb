d1c5cd9e0cb2b859c8158ad094aa5472e5cb5976
come mellerio <come.mellerio@free.fr>
2018-03-20 16:15:48 +0100

    index cards

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -8,4 +8,19 @@ class RacesController < ApplicationController
     @race = policy_scope(Race.find(params[:id]))
   end
 
+  # def create
+  #   @race = Race.new(product_params)
+  #   @race.user = current_user
+  #   if @race.save
+  #     redirect_to product_path(@race)
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+
+  #   def product_params
+  #     params.require(:race).permit(:title, :category, :price_per_day, :address, :description, :photo)
+  #   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Race.destroy_all
 
-puts "seeding race1"
-race1 = Race.new(name:"Triathlon de Deauville", sport:"Triathlon", format:"Triathlon Longue Distance", starts_at:"Sat, 17 Mar 2018 12:00:00 UTC +00:00", address:"Deauville", year:"2018", description:"Triathlon de Deauville Description", url:"https://triathlondeauville.com/", registration_url:"https://www.njuko.net/tid2018/select_competition", available_slots:1000, distance_swim:1.9, distance_bike:90, distance_run:21.1, vertical_ascent_bike:1500, vertical_ascent_run:100)
-race1.save
-puts "seed race1 complete"
+sport = ["Triathlon", "Running", "Road Cycling", "Obstacle", "Swimming", "Mountain Biking", "Duathlon", "SwimRun"]
+address = ["Paris", "Lyon", "Marseilles", "Bordeaux", "Deauville", "Lille"]
+format = ["Super Sprint", "Sprint", "Olympic", "Half Ironman", "Ironman"]
+starts_at = [DateTime.current, DateTime.current + 1.day, DateTime.current + 1.week, DateTime.current + 1.month]
+year = ["2018"]
+description = ["Triathlon de Deauville Description"]
+url = ["https://triathlondeauville.com/"]
+registration_url = ["https://www.njuko.net/tid2018/select_competition"]
+available_slots = [1000]
+distance_swim = [1.9]
+distance_bike = [90]
+distance_run = [21.1]
+vertical_ascent_bike = [1500]
+vertical_ascent_run = [100]
+
+10.times do |race|
+  race = Race.new(name:"#{sport.sample} de #{address.sample}", sport: sport.sample, format:format.sample, starts_at: starts_at.sample, address: address.sample, year: year.sample, description: description.sample, url: url.sample, registration_url: registration_url.sample, available_slots: 1000, distance_swim: 1.9, distance_bike: 90, distance_run: 21.1, vertical_ascent_bike: 1500, vertical_ascent_run: 100)
+  race.save
+  puts "Saving #{race.name}"
+end
+
+
