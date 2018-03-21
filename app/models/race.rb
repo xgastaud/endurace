4ae467ffa8437e1ef904bb8c5b9589a5a8c12d5c
come mellerio <come.mellerio@free.fr>
2018-03-21 10:41:49 +0100

    Fix races controller + index

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,4 @@
 class Race < ApplicationRecord
   has_many :participations, dependent: :destroy
+  mount_uploader :photo, PhotoUploader
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,6 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   def default_url(*args)
-      ActionController::Base.helpers.asset_path([version_name, "race_default.jpg"].compact.join('_'))
+      ActionController::Base.helpers.asset_path([version_name, "race-default.jpg"].compact.join('_'))
   end
 end
