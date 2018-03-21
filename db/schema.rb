65fa092e89dc1b6ca58c335862ff3525355b19fc
come mellerio <come.mellerio@free.fr>
2018-03-21 19:42:35 +0100

    indexv3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320225219) do
+ActiveRecord::Schema.define(version: 20180321134758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +42,10 @@ ActiveRecord::Schema.define(version: 20180320225219) do
     t.float "vertical_ascent_run"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
