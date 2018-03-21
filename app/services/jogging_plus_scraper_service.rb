bcb4d1354a58cb0fdf5395f930f5dbb5cf2a9c09
Baptiste Chebassier <baptiste.chebassier28@gmail.com>
2018-03-21 18:14:27 +0100

    data jp done

--- app/services/jogging_plus_scraper_service.rb
+++ app/services/jogging_plus_scraper_service.rb
@@ -8,51 +8,83 @@ class JoggingPlusScraperService
   end
 
   def scrape
-    # puts "hello"
-    # scrape
+    # @base = "http://www.jogging-plus.com/calendrier/calendrier-triathlon-france/"
     html_file = open(@base_url).read
     html_doc = Nokogiri::HTML(html_file)
     urls = []
     html_doc.search('.tablo1 a').each do |element|
-       # puts element.search('.lienorange').text.strip
-       urls << element.attribute('href').value
+     # puts element.search('.lienorange').text.strip
+      urls << element.attribute('href').value
     end
+
     urls.each do |url|
+      # url = "http://www.jogging-plus.com/presentation-courses-trails/trail-atlantisport-environnement-loire-atlantique/"
       html_show = open(url).read
       html_doc_show = Nokogiri::HTML(html_show)
-      bloc =[]
-      location =[]
+      bloc = []
+      location = []
       address = ""
-      puts url_inscription = html_doc.search("#bloc-gauche3 .lienorange").attribute('href').value
-      html_doc.search("#bloc-gauche2 #bloc-info-label").each do |element|
+
+      link_registration = html_doc_show.search("#bloc-gauche2 .btn_inscr_event")
+      url_registration = link_registration.attribute('href').value unless link_registration.empty?
+
+      link_photo = html_doc_show.search("#myCarousel .item img")
+      photo = link_photo.attribute('src').value unless link_photo.empty?
+
+      link_inscription = html_doc_show.search("#bloc-gauche3 .lienorange")
+      url_organizer =  link_inscription.attribute('href').value unless link_inscription.empty?
+      "url orga: #{url_organizer}"
+      "url registration: #{url_registration}"
+      html_doc_show.search("#bloc-gauche2 #bloc-info-label").each do |element|
         bloc << element
       end
-      puts places_total = bloc[1].text
-      puts name = html_doc.search(".td-post-title .entry-title").text.strip
-      puts date = html_doc.search("#bloc-date-fiche").text.strip
-      x = html_doc.search("#bloc-gauche2 #bloc-info-valeur").text.strip.split
+      places_total = bloc[1].text
+      name = html_doc_show.search(".td-post-title .entry-title").text.strip
+      date = html_doc_show.search("#bloc-date-fiche").text.strip
+      x = html_doc_show.search("#bloc-gauche2 #bloc-info-valeur").text.strip.split
       x.each_with_index do |element, index|
         if index < 3
           location << element
           address = location.join
         end
       end
-      puts address
-      puts html_doc.search("#bloc-gauche2 br").text.strip
-      html_doc.search("#bloc-gauche2 br").each do |element|
-        puts element.text
-      end
-      puts description = html_doc.search('em').first.text.strip
-      # puts html_doc.attribute("br").value
-      race1 = Race.new(name: name, sport: "running", format: "marathon", starts_at: date, address: address, year: nil, description: description, url: nil, registration_url: url_inscription, available_slots: places_total, distance_swim: nil,
-      distance_bike: nil,
-      distance_run: nil,
-      vertical_ascent_bike: nil,
-      vertical_ascent_run: nil)
+      description = html_doc_show.search('em').first.text.strip
+      race1 = Race.new(
+        name: name,
+        sport: categories[:sport],
+        format: categories[:format],
+        starts_at: date,
+        address: address,
+        description: description,
+        url: url_organizer,
+        registration_url: url_registration,
+        available_slots: places_total,
+        distance_swim: nil,
+        distance_bike: nil,
+        distance_run: nil,
+        vertical_ascent_bike: nil,
+        vertical_ascent_run: nil,
+        image_race: photo,
+      )
+      race1.year = race1.starts_at.year
       race1.save
       puts "#{race1.name} saved!"
     end
-    # create instances
-    # save
+  end
+
+  def categories
+    if @category == "/marathons/france"
+      {sport: "Running",
+       format: "Marathon"}
+    elsif @category == "calendrier-triathlon-france"
+      {sport:"Triathlon",
+      format: nil}
+    elsif @category == "courses-5-10-15-km/france"
+      {sport: "Running",
+       format: "5k"}
+    elsif @category == "semi-marathons/france"
+      {sport: "Running",
+       format: "Semi-marathon"}
+    end
   end
 end
--- lib/tasks/scraper.rake
+++ lib/tasks/scraper.rake
@@ -1,10 +1,9 @@
 namespace :scraper do
   desc "scrape jogging plus"
   task jogging_plus: :environment do
-    # categories = [....]
-    #categories = ["marathons/france"]
-    # categories.each do |category|
-    # end
-
-    JoggingPlusScraperService.new('marathons/france').scrape
+    categories = ["/marathons/france", "calendrier-triathlon-france", "courses-5-10-15-km/france", "semi-marathons/france"]
+    categories.each do |category|
+      JoggingPlusScraperService.new(category).scrape
+    end
   end
+end
