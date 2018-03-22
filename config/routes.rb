18a9e4d68091cf04294ec175d3864a1d2be37bd3
Xavier Gastaud <xgastaud@gmail.com>
2018-03-22 10:09:54 +0100

    privacy and terms of services pages for FB

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :races, only: [:index, :show]
+  get 'privacy', to: 'pages#privacy', as: :privacy
+  get 'tos', to: 'pages#tos', as: :tos
 end
