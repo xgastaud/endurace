18748c017d00fa07b4723518d832bf778014c795
Xavier Gastaud <xgastaud@gmail.com>
2018-03-22 17:52:02 +0100

    seeding improvements

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,4 +4,19 @@ class Race < ApplicationRecord
   validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  FORMATS_SPORTS = {
+    "Triathlon" => ["Ironman", "Longue Distance - Half-Ironman", "Courte Distance - Olympique", "Sprint", "Super Sprint", "Autre"],
+    "Running" => ["Marathon", "Semi-marathon", "15km et moins", "Autre"]
+    # "Swimrun" => "Long",
+    # "Cyclisme" => "Gran Fondo",
+  }
+
+  validates :sport, inclusion: { in: FORMATS_SPORTS.keys, allow_blank: false }
+  validate :format_consistent_with_sport
+
+  def format_consistent_with_sport
+    if !(FORMATS_SPORTS[self.sport] || []).include?(self.format)
+      errors.add(:format, "is not included in the list")
+    end
+  end
 end
