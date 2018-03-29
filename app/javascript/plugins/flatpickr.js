caa2dab2fd97d092e95ae5c6ea83e2795f12c54c
Emile Sautet <emilesautet@gmail.com>
2018-03-29 20:16:38 +0200

    début de fixing

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,9 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {})
+// flatpickr(".datepicker", {})
+
+flatpickr("#range_start", {
+  mode: "range"
+})
